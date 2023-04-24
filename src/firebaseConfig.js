b9f6e97838913e7414351e4c2fe304d1172b9627
Rahul Saxena <rahulsaxena2021@hotmail.com>
2023-04-24 16:33:55 +0530

    basic structure has been set up
    installed vite using npm
    created an app on firebase site and pasted the firebase sdk inside the firebase config file
    - started working on Login and its components - 2 dir ( components, Pages )

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBkfQ1su4fw8ZiIDBtK15mxb0OTO6a_Tu8',
+  authDomain: 'linkedin-clone-850da.firebaseapp.com',
+  projectId: 'linkedin-clone-850da',
+  storageBucket: 'linkedin-clone-850da.appspot.com',
+  messagingSenderId: '174113760754',
+  appId: '1:174113760754:web:7aa59d2dfe45f3578f376b',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
